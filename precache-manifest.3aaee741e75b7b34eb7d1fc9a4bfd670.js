52a5c109e394e97d52b0b8e8ef5c75b0a3cfdc04
Roman <rmnbrtshk@gmail.com>
2019-06-14 11:38:48 +0300

    Updates

---
+++ precache-manifest.3aaee741e75b7b34eb7d1fc9a4bfd670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed72c4615e8c96ace6eb494da6995636",
+    "url": "/bookshop/index.html"
+  },
+  {
+    "revision": "4ac175c008a198c410a9",
+    "url": "/bookshop/static/css/main.8fe12aa2.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/bookshop/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "4ac175c008a198c410a9",
+    "url": "/bookshop/static/js/main.3cb00847.chunk.js"
+  },
+  {
+    "revision": "878529ee6d5de803391b",
+    "url": "/bookshop/static/js/runtime~main.e22bd24b.js"
+  }
+]);
