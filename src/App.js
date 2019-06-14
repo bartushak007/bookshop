375407c416806af61d08dee51def5c301044aad4
Roman <rmnbrtshk@gmail.com>
2019-06-14 11:35:30 +0300

    create react project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import useFetch from './hooks/useFetch';
+import { resolvePath } from './helpers/index';
+
+const App= () => {
+
+  // const { data, loading} = useFetch(resolvePath('data.json'));
+  
+  return (
+    <div className="App">
+      HELLO      
+    </div>
+  );
+}
+
+export default App;
